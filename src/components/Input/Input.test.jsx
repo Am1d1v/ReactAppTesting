16a12e1a6630b2cbe294d48a8e3a014527963e7b
Am1d1v <dimadiman1adima@gmail.com>
2024-04-29 14:41:12 +0300

    Users Input Test

--- src/components/Input/Input.test.jsx
+++ src/components/Input/Input.test.jsx
@@ -6,14 +6,28 @@ const testPlaceHolder = 'Test Placeholder'
 describe('User Input Test', () => {
 
     it('Shloud render the input', () => {
-        render(<Input placeholder={testPlaceHolder}/>);
+        render(<Input placeholder={testPlaceHolder} />);
         
         expect(screen.getByPlaceholderText(testPlaceHolder)).toBeInTheDocument();
     });
 
-    it.todo('Shloud render the input with correct type');
+    it('Shloud render the input with correct type', () => {
+        render(<Input placeholder={testPlaceHolder} type="checkbox" />);
 
-    it.todo('Shloud render the input with correct classname');
+        expect(screen.getByRole('checkbox')).toBeInTheDocument();
+    });
+
+    it('Shloud render the input with correct classname', () => {
+        const {container} = render(
+        <Input 
+          placeholder={testPlaceHolder}
+          inputClassName = "inputTest"
+          containerClassName = "containerTest"  
+        />)
+
+        const containerEl = container.querySelector('.formControl.containerTest')
+        expect(containerEl).toBeInTheDocument();
+    });
 
 
 });
