f6b0d4c3e4a41258eeeff71ee5925491a6a8f939
Am1d1v <dimadiman1adima@gmail.com>
2024-05-09 17:32:58 +0300

    React Testing Library. Form fireEvent onSubmit

--- src/components/Form/Form.test.jsx
+++ src/components/Form/Form.test.jsx
@@ -12,6 +12,22 @@ describe('User Form test', () => {
         </Form>)
 
         expect(getByTestId('form-id')).toBeInTheDocument();
+        expect(container.querySelector('form')).toBeInTheDocument();
+    });
+
+    it('Should invoke the onSubmit callback', () => {
+        const onSubmit = jest.fn();
+
+        const {container} = render(<Form onSubmit={onSubmit}/>);
+        const formHolder = container.querySelector('form');
+
+        fireEvent.submit(formHolder);
+
+        // Check that onSubmit have been called
+        expect(onSubmit).toHaveBeenCalled();
+
+        // Check that onSubmit have been called 1 time
+        expect(onSubmit).toHaveBeenCalledTimes(1);
     });
 
 });
