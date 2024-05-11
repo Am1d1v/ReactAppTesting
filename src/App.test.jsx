036b3c68985f5b29827e59f8316ad685d49dc48a
Am1d1v <dimadiman1adima@gmail.com>
2024-05-11 14:34:46 +0300

    React testing library. App test

---
+++ src/App.test.jsx
@@ -0,0 +1,10 @@
+import App from "./App";
+import { render, screen, fireEvent, act, waitFor } from "@testing-library/react";
+
+describe('App Integrational test', () => {
+
+    it.todo('Should render App with form elements and a title');
+    it.todo('Should render error message when form was submit with a weak password');
+    it.todo('Should render successfull messafe after successfull submit');
+
+});
