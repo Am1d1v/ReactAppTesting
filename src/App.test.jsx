29aa8de8af6693da585a830ef6212d72ec11ea79
Am1d1v <dimadiman1adima@gmail.com>
2024-05-11 14:43:00 +0300

    Test case render App with form elements and a title added

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -3,8 +3,24 @@ import { render, screen, fireEvent, act, waitFor } from "@testing-library/react"
 
 describe('App Integrational test', () => {
 
-    it.todo('Should render App with form elements and a title');
+    it('Should render App with form elements and a title', () => {
+        const {container} = render(<App />);
+
+        expect(screen.getByTestId('app')).toBeInTheDocument();
+
+        const userName = screen.getByLabelText(/User name/);
+        const passwordInput = screen.getByLabelText(/Password/);
+        const submitButton = screen.getByRole('button', {name: /Create user/});
+        const title = container.querySelector('h1');
+
+        expect(userName).toBeInTheDocument();
+        expect(passwordInput).toBeInTheDocument();
+        expect(submitButton).toBeInTheDocument();
+        expect(title).toBeInTheDocument();
+    });
+
     it.todo('Should render error message when form was submit with a weak password');
+
     it.todo('Should render successfull messafe after successfull submit');
 
 });
